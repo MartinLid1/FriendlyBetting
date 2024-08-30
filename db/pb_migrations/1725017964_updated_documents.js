c74379e8378c3ff58a40e885908db2d40e08c88f
Fredrik Öhrström <fredrik@ohrstrom.io>
2024-08-30 15:44:16 +0200

    add migrations

---
+++ db/pb_migrations/1725017964_updated_documents.js
@@ -0,0 +1,77 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("24j7x9ot48riaoh")
+
+  collection.name = "contracts"
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zydgtzgs",
+    "name": "owner",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "8znu0hnx",
+    "name": "participants",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": null,
+      "displayFields": null
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "jwt4fzlu",
+    "name": "content",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("24j7x9ot48riaoh")
+
+  collection.name = "documents"
+
+  // remove
+  collection.schema.removeField("zydgtzgs")
+
+  // remove
+  collection.schema.removeField("8znu0hnx")
+
+  // remove
+  collection.schema.removeField("jwt4fzlu")
+
+  return dao.saveCollection(collection)
+})
